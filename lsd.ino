10dd4a9659c560634edd2a4ff9316c5d7956bea1
Gogo Lejzr <gogo.lejzr@gmail.com>
2018-08-13 20:42:44 +0200

    test char rendering

--- lsd.ino
+++ lsd.ino
@@ -1,9 +1,39 @@
 #include <inttypes.h>
 #include "Print.h"
 
-class LiquidCrystal : public Print {
-  LiquidCrystal();
+class LSD{
+  public:
+    LSD();
+    void render();
+
+  private:
+    static const uint8_t _cols = 16;
+
+    uint8_t _upper_row[_cols];
+    uint8_t _lower_row[_cols];
 };
+
+LSD::LSD() {
+  for(int i = 0; i < _cols; i++){
+    _upper_row[i] = 'u';
+    _lower_row[i] = 'l';
+  }
+}
+
+void LSD::render() {
+  for(int i = 0; i < _cols; i++)
+    Serial.print((char) _upper_row[i]);
+
+  Serial.print("  ");
+  
+  for(int i = 0; i < _cols; i++)
+    Serial.print((char) _lower_row[i]);
+
+  Serial.print("\r");
+
+}
+
+LSD lsd;
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,10 +42,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);
-  Serial.println("test test");                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  lsd.render();
   delay(1000);
 }
