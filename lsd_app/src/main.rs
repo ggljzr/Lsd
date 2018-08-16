c293f1410ff1438c6c2f834a350916549624a139
Gogo Lejzr <gogo.lejzr@gmail.com>
2018-08-16 03:24:13 +0200

    added expected dependencies, started working on lsd_app

--- lsd_app/src/main.rs
+++ lsd_app/src/main.rs
@@ -1,18 +1,51 @@
-extern crate piston_window;
+extern crate serialport;
 
-use piston_window::*;
+use std::io::{self, Write};
+use std::time::Duration;
+
+use serialport::prelude::*;
+
+enum Command {
+    INIT,
+}
+
+fn parse_command(data: &Vec<u8>) -> Result<Command, io::Error> {
+    println!("{:?}", data);
+    Ok(Command::INIT)
+}
 
 fn main() {
-    let mut window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [640, 480])
-        .exit_on_esc(true).build().unwrap();
-    while let Some(event) = window.next() {
-        window.draw_2d(&event, |context, graphics| {
-            clear([1.0; 4], graphics);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0],
-                      context.transform,
-                      graphics);
-        });
+    let mut port_name = "COM3".to_string();
+    let mut baud_rate = "9600".to_string();
+
+    let mut settings: SerialPortSettings = Default::default();
+    settings.timeout = Duration::from_millis(10);
+    if let Ok(rate) = baud_rate.parse::<u32>() {
+        settings.baud_rate = rate.into();
+    } else {
+        eprintln!("Error: Invalid baud rate '{}' specified", baud_rate);
+        ::std::process::exit(1);
+    }
+
+    match serialport::open_with_settings(&port_name, &settings) {
+        Ok(mut port) => {
+            let mut serial_buf: Vec<u8> = vec![0; 16];
+            println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
+            loop {
+                match port.read(serial_buf.as_mut_slice()) {
+                    Ok(t) => { parse_command(&serial_buf); () },
+                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            }
+        },
+        Err(e) => {
+            eprintln!(
+                "Failed to open \"{}\". Error: {}",
+                port_name,
+                e
+            );
+            ::std::process::exit(1);
+        },
     }
 }
